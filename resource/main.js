cbdc3bed495a00cc9558279f72fae692af845176
MagmaChocolate <gg845@hotmail.co.jp>
2018-02-27 16:49:13 +0900

    [update] add show merge function

--- resource/main.js
+++ resource/main.js
@@ -26,20 +26,20 @@ function read(e,F) {
     if(F === 'A') domA = perseXML(text);
     if(F === 'B') domB = perseXML(text);
 
-  if(domA !== undefined && domB !== undefined){
-    if(!checkFixture(domA,domB))console.log('２つのファイルのフィクスチャーが一致しません。マージに失敗しました。');
-    mergedDom = mergeDom(domA,domB);
+    if(domA !== undefined && domB !== undefined){
+      if(!checkFixture(domA,domB))console.log('２つのファイルのフィクスチャーが一致しません。マージに失敗しました。');
+      mergedDom = mergeDom(domA,domB);
 
-    // http://www.openspc2.org/reibun/javascript2/XML/convert/0002/index.html
-    let xs = new XMLSerializer();
-    let xmlText =  xs.serializeToString(mergedDom);
+      // http://www.openspc2.org/reibun/javascript2/XML/convert/0002/index.html
+      let xs = new XMLSerializer();
+      let xmlText =  xs.serializeToString(mergedDom);
 
-    // http://kurukurupapa.hatenablog.com/entry/20121006/1349522062
-    let blob = new Blob([ xmlText ], { "type" : "text/xml" });
-    window.URL = window.URL || window.webkitURL;
-    document.getElementById('download').setAttribute('download', "merged.qxw");
-    document.getElementById('download').setAttribute('href', window.URL.createObjectURL(blob));
-  }
+      // http://kurukurupapa.hatenablog.com/entry/20121006/1349522062
+      let blob = new Blob([ xmlText ], { "type" : "text/xml" });
+      window.URL = window.URL || window.webkitURL;
+      document.getElementById('download').setAttribute('download', "merged.qxw");
+      document.getElementById('download').setAttribute('href', window.URL.createObjectURL(blob));
+    }
 
   }
   fr.readAsText(file); // ファイルをテキストとして読み込む
@@ -196,4 +196,141 @@ function intoFolder(dom,folderName){
     }
   }
   return dom;
+}
+
+
+function listShow_e(){
+  let tmp = listShow(domA);
+  for (index in tmp){
+    document.getElementById('text').textContent = document.getElementById('text').textContent + tmp[index].name +':'+tmp[index].id +"\n";
+  }
+}
+/**
+ * Showファンクションの一覧を表示する
+ * @method listShow
+ * @param  {[type]} dom [description]
+ * @return {[type]}     [description]
+ */
+function listShow(dom){
+  let out = [];
+  let arr = dom.querySelectorAll('Engine > Function[Type="Show"]');
+  const N = dom.querySelectorAll('Engine > Function[Type="Show"]').length;
+  for(let i = 0;i < N;i++){
+    out[i] = {
+      'name': arr.item(i).getAttribute('Name'),
+      'id': arr.item(i).getAttribute('ID')
+    }
+  }
+  return out;
+}
+
+
+function testFixture(){
+  if(checkFixture(domA,domB)){
+    window.alert('fixture OK ');
+  }else{
+    window.alert('fixture No');
+  }
+}
+
+
+/**
+ * データ内の２つのショーデータを１つに結合します。
+ * 同じデータ内のショーであることが前提
+ * @method mergeShow
+ * @param  {int}  first  結合されるFunction No.
+ * @param  {int}  second 連結するFunction No.
+ * @param {string} nameAdd追記する名前
+ * @return {XML_DOM, false}         引数が不正な場合はfalse
+ */
+function mergeShow(first,second, nameAdd){
+  let dom = domA;
+  const BLANK_TIME = 1000; //1s
+  // 有効性チェック
+  if(dom.querySelectorAll('Engine > Function[ID="'+first+'"]').item(0).getAttribute('Type') !== 'Show'){
+    console.warn('mergeShow()に渡された引数{first}が不正です');
+    return false;
+  }
+  if(dom.querySelectorAll('Engine > Function[ID="'+second+'"]').item(0).getAttribute('Type') !== 'Show'){
+    console.warn('mergeShow()に渡された引数{second}が不正です');
+    return false;
+  }
+
+  // 秒数バイアスの決定
+  let timeBias = 0;
+  let N = dom.querySelectorAll('Engine > Function[ID="'+first+'"] > Track > ShowFunction').length;
+  for (let i = 0;i < N;i++){
+    num =  Number(dom.querySelectorAll('Engine > Function[ID="'+first+'"]').item(0).getElementsByTagName('ShowFunction').item(i).getAttribute('StartTime')) + Number(dom.querySelectorAll('Engine > Function[ID="'+first+'"]').item(0).getElementsByTagName('ShowFunction').item(i).getAttribute('Duration'));
+    if(num > timeBias){
+      timeBias = Number(dom.querySelectorAll('Engine > Function[ID="'+first+'"]').item(0).getElementsByTagName('ShowFunction').item(i).getAttribute('StartTime')) + Number(dom.querySelectorAll('Engine > Function[ID="'+first+'"]').item(0).getElementsByTagName('ShowFunction').item(i).getAttribute('Duration'));
+    }
+  }
+  timeBias += BLANK_TIME;
+  // 値のシフト
+  N = dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track > ShowFunction').length;
+  for (let i = 0; i < N;i++){
+    dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track > ShowFunction').item(i).setAttribute('StartTime', Number(dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track > ShowFunction').item(i).getAttribute('StartTime')) + timeBias);
+  }
+
+  // Track ID bias
+  let idBias = 0;
+  N = dom.querySelectorAll('Engine > Function[ID="'+first+'"] >Track').length;
+  for(let i = 0;i < N;i++){
+    if(dom.querySelectorAll('Engine > Function[ID="'+first+'"] > Track').item(i).getAttribute('ID') > idBias){
+      idBias = Number(dom.querySelectorAll('Engine > Function[ID="'+first+'"] > Track').item(i).getAttribute('ID'));
+    }
+  }
+  idBias += 1;
+ // 値のシフト
+  N = dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track').length;
+  for(let i = 0;i < N;i++){
+    // idシフト
+    dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track').item(i).setAttribute('ID', Number(dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track').item(i).getAttribute('ID')) + idBias);
+    // 名前追記
+    dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track').item(i).setAttribute('Name', nameAdd + dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track').item(i).getAttribute('Name'));
+  }
+
+
+  // 結合
+  // first に指定したShowに追記
+  N = dom.querySelectorAll('Engine > Function[ID="'+second+'"] >Track').length;
+  let temp = dom.querySelectorAll('Engine > Function[ID="'+second+'"] > Track');
+  for(let i = 0; i< N;i++){
+    dom.querySelector('Engine > Function[ID="'+first+'"]').appendChild(temp.item(i));
+  }
+
+
+  domA = dom
+}
+
+
+/**
+ * ダウンロード用blobを生成する
+ * @method createFile
+ * @param  {XML_DOM}   dom [description]
+ */
+function createFile(dom){
+  // http://www.openspc2.org/reibun/javascript2/XML/convert/0002/index.html
+  let xs = new XMLSerializer();
+  let xmlText =  xs.serializeToString(dom);
+
+  // http://kurukurupapa.hatenablog.com/entry/20121006/1349522062
+  let blob = new Blob([ xmlText ], { "type" : "text/xml" });
+  window.URL = window.URL || window.webkitURL;
+  document.getElementById('download').setAttribute('download', "merged.qxw");
+  document.getElementById('download').setAttribute('href', window.URL.createObjectURL(blob));
+}
+
+
+/**
+ * ショーデータをずらす
+ * @method shiftShow
+ * @param {int}  id Functoin ID
+ * @param  {int}  ms [description]
+ */
+function shiftShow(id,ms){
+  let N = domA.querySelectorAll('Engine > Function[ID="'+id+'"] > Track > ShowFunction').length;
+  for (let i = 0; i < N;i++){
+    domA.querySelectorAll('Engine > Function[ID="'+id+'"] > Track > ShowFunction').item(i).setAttribute('StartTime', Number(domA.querySelectorAll('Engine > Function[ID="'+id+'"] > Track > ShowFunction').item(i).getAttribute('StartTime')) + ms);
+  }
 }
